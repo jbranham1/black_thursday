5928e706b511a84d9297446534a042289cc93f4d
Alex Schwartz <aschwartz@detaso.com>
2021-01-05 08:36:11 -0600

    Update syntax because hound

--- test/item_test.rb
+++ test/item_test.rb
@@ -6,13 +6,13 @@ require './lib/item'
 class ItemTest < Minitest::Test
   def setup
     @pencil = Item.new({
-      id:           1,
-      name:         'Pencil',
-      description:  'You can use it to write things.',
-      unit_price:   BigDecimal.new(10.99, 4),
-      created_at:   Time.now,
-      updated_at:   Time.now,
-      merchant_id:  2
+      id: 1,
+      name: 'Pencil',
+      description: 'You can use it to write things.',
+      unit_price: BigDecimal(10.99, 4),
+      created_at: Time.now,
+      updated_at: Time.now,
+      merchant_id: 2
     })
   end
 
@@ -24,7 +24,7 @@ class ItemTest < Minitest::Test
     assert_equal 1, @pencil.id
     assert_equal 'Pencil', @pencil.name
     assert_equal 'You can use it to write things.', @pencil.description
-    assert_equal BigDecimal.new(10.99, 4), @pencil.unit_price
+    assert_equal BigDecimal(10.99, 4), @pencil.unit_price
     assert_instance_of Time, @pencil.created_at
     assert_instance_of Time, @pencil.updated_at
     assert_equal 2, @pencil.merchant_id
