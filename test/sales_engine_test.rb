59527cac65e28d79404ccde48a888c2adf5d8bf8
Jenny Branham <branhamjenny@gmail.com>
2021-01-05 08:05:23 -0600

    Hound changes

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -5,8 +5,8 @@ require './lib/sales_engine'
 class SalesEngineTest < Minitest::Test
   def test_it_exists
     files = {
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
+      items: './data/items.csv',
+      merchants: './data/merchants.csv'
     }
     sales_engine = SalesEngine.new(files)
 
@@ -15,10 +15,10 @@ class SalesEngineTest < Minitest::Test
 
   def test_from_csv
     files = {
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
+      items: './data/items.csv',
+      merchants: './data/merchants.csv'
     }
-    
+
     sales_engine = SalesEngine.from_csv(files)
     assert_instance_of ItemRepository, sales_engine.items
     assert_instance_of MerchantRepository, sales_engine.merchants
