4b055c195e9518387f278da9a5762c9fcca59542
Angel Breaux <abreaux@detaso.com>
2021-01-05 15:44:33 -0600

    Add item repository frame work

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,5 +1,29 @@
-class ItemRepository
+require_relative 'item'
 
-  def initialize(csv)
+class ItemRepository < Item
+  attr_reader :file
+
+  def initialize(file)
+    @file = file
+  end
+
+  def items
+    @items ||= build_items
+  end
+
+  def build_items
+    parameters = { headers: true, header_converters: :symbol }
+    CSV.open(@file, parameters).map do |row|
+      info = {
+        id: row[:id].to_i,
+        name: row[:name],
+        description: row[:description],
+        unit_price: BigDecimal(row[:unit_price], 4),
+        created_at: Time.parse(row[:created_at]),
+        updated_at: Time.parse(row[:updated_at]),
+        merchant_id: row[:merchant_id].to_i
+      }
+      Item.new(info)
+    end
   end
 end
