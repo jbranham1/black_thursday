d30b5f6e4ddc5f36ab22af3a6b3642e6765f9a06
Jenny Branham <branhamjenny@gmail.com>
2021-01-05 11:50:50 -0600

    Fix hound errors

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -5,19 +5,19 @@ require './lib/merchant_repository'
 
 class MerchantRepositoryTest < Minitest::Test
   def test_it_exists
-    file = CSV.open './data/test_merchant.csv',
-    headers: true, header_converters: :symbol
+    parameters = {headers: true, header_converters: :symbol}
+    file = CSV.open './data/test_merchant.csv', parameters
     repo = MerchantRepository.new(file)
 
     assert_instance_of MerchantRepository, repo
   end
 
   def test_build_merchants
-    file = CSV.open './data/test_merchant.csv',
-    headers: true, header_converters: :symbol
+    parameters = {headers: true, header_converters: :symbol}
+    file = CSV.open './data/test_merchant.csv', parameters
     repo = MerchantRepository.new(file)
-    merchant1 = Merchant.new({ id: 12334105, name: 'Shopin1901' })
-    merchant2 = Merchant.new({ id: 12334112, name: 'Candisart' })
+    merchant1 = Merchant.new(id: 12334105, name: 'Shopin1901')
+    merchant2 = Merchant.new(id: 12334112, name: 'Candisart')
 
     assert_equal [merchant1, merchant2], repo.build_merchants
   end
