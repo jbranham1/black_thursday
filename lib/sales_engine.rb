860b8cef810782a70ed5aeef288bcd377e336ac2
Jenny Branham <branhamjenny@gmail.com>
2021-01-05 10:35:38 -0600

    Add to load_file

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,13 +1,19 @@
+require_relative "MerchantRepository"
 class SalesEngine
   attr_reader :merchants,
               :items
 
   def initialize(files)
-    @merchants = MerchantRepository.new(files[:merchants])
-    @items = ItemRepository.new(files[:merchants])
+    @merchants = load_file(files[:merchants], MerchantRepository)
+    @items = load_file(files[:items], ItemRepository)
   end
 
-  def self.from_csv(data)
-    new(data)
+  def self.from_csv(files)
+    new(files)
+  end
+
+  def load_file(file_name, repository)
+    @file = CSV.open file_name, headers: true, header_converters: :symbol
+    repository.new(@file)
   end
 end
