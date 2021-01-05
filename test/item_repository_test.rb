6a732e67c5efaa3690d1fe2a50553279dcfc980b
Angel Breaux <abreaux@detaso.com>
2021-01-05 13:33:10 -0600

    WIP, add more tests to item repository test file

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -1,13 +1,27 @@
+require './test/test_helper'
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'csv'
+require 'bigdecimal'
 require './lib/item_repository'
 
 class ItemRepositoryTest < Minitest::Test
+  def setup
+    @file = CSV.open './data/test_item.csv', headers: true, header_converters: :symbol
+  end
+
   def test_it_exists
-    file = CSV.open './data/test_item.csv', headers: true, header_converters: :symbol
-    repo = ItemRepository.new(file)
+    repo = ItemRepository.new(@file)
 
     assert_instance_of ItemRepository, repo
+  end
+
+  def test_build_items
+    repo = ItemRepository.new(@file)
+    repo.build_items
+
+    repo.items.all? do |item|
+      assert_instance_of ItemRepository, item
+    end
   end
 end
