6fbc60600de3edbfe6d8c86754bd6a0c02d2da87
Alex Schwartz <aschwartz@detaso.com>
2021-01-09 10:06:44 -0600

    Rework items test

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -1,10 +1,27 @@
 require './test/test_helper'
+require 'bigdecimal'
 require './lib/merchant'
+require './lib/item'
 
 class MerchantTest < Minitest::Test
   def setup
     @repository = mock
     @merchant = Merchant.new({ id: 5, name: 'Turing School' }, @repository)
+    @sample_item = create_item
+  end
+
+  def create_item
+    Item.new(
+      {
+        id: 1,
+        name: 'Pencil',
+        description: 'You can use it to write things.',
+        unit_price: BigDecimal(1099, 4),
+        created_at: Time.new(2021, 1, 1, 8, 0, 0),
+        updated_at: Time.new(2021, 1, 1, 8, 0, 0),
+        merchant_id: 2
+      }, mock
+    )
   end
 
   def test_it_exists
@@ -21,5 +38,11 @@ class MerchantTest < Minitest::Test
     @merchant.update(name: 'Turing School Updated')
 
     assert_equal 'Turing School Updated', @merchant.name
+  end
+
+  def test_can_retrieve_items
+    @repository.stubs(:items_by_merchant_id).with(@merchant.id).returns([@sample_item])
+
+    assert_equal [@sample_item], @merchant.items
   end
 end
